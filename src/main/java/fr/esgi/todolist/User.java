34bdac5fa54fe94f8c29346500d644b059fe77a1
Marwan <marwan.boubchir@outlook.fr>
2022-06-22 11:09:44 +0200

    Ajout de is Valid

--- src/main/java/fr/esgi/todolist/User.java
+++ src/main/java/fr/esgi/todolist/User.java
@@ -1,15 +1,34 @@
 package fr.esgi.todolist;
 
+import java.time.LocalDate;
+
 public class User {
     String name;
     String lastname;
     String email;
-    Integer age;
+    LocalDate age;
 
-    public User(String name, String lastname, String email, Integer age) {
+    public User(String name, String lastname, String email, LocalDate age) {
         this.name = name;
         this.lastname = lastname;
         this.email = email;
         this.age = age;
+    }
+
+    public boolean isValid(){
+        if (this.name != null
+                && this.lastname != null
+                && this.age != null
+                && this.email != null ){
+            if(LocalDate.now().minusYears(13).isAfter(this.age)){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        else{
+            return false;
+        }
     }
 }
