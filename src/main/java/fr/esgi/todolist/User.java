e5f025106f2f46cd1d019b29fdc78e10bfedf0c1
Shan AZIZ <shan.azizh@gmail.com>
2022-06-22 11:14:42 +0200

    custom email validator

--- src/main/java/fr/esgi/todolist/User.java
+++ src/main/java/fr/esgi/todolist/User.java
@@ -12,4 +12,6 @@ public class User {
         this.email = email;
         this.age = age;
     }
+
+
 }
---
+++ src/main/java/fr/esgi/todolist/validators/CustomEmailValidator.java
@@ -0,0 +1,9 @@
+package fr.esgi.todolist.validators;
+
+import jdk.jshell.spi.ExecutionControl.NotImplementedException;
+
+public class CustomEmailValidator {
+    public static boolean validate(String email) throws NotImplementedException {
+        throw new NotImplementedException("Not implemented yet");
+    }
+}
